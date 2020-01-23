53f5a648c2461fa6bc52c994825a350f60838f2f
Tom Duszyk <tduszyk@gmail.com>
2020-01-23 14:47:44 -0500

    added socket

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-import os, sys, time
-hosts = open("scope.csv", "r")
-failures = open('failures.txt', 'a')
+# add shebang here #!/usr/bin/python2
 
-for hostname in hosts:
-     response = os.system("ping -c 1 " + hostname + " >/dev/null 2>&1")
+import os, socket
 
-     if response == 0:
-             print(hostname, 'Ping successful!')
-             sys.exit(0)
-     else:
-             print(hostname, 'Ping unsuccessful.')
-             failures.write(hostname)
-             sys.exit(1)
+
+# Read files
+scope_file = open('scope.csv', 'r')
+hosts = scope_file.read().splitlines()
+
+# Check if hosts are available by connecting to port 22
+for host in hosts:
+    # Create socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex((host, 22))
+    if result == 0:
+        pprint (host, " is UP")
+    else:
+        pprint (host, " no SSH")
