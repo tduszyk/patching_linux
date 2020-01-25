db03d588e9f734ac453c86979d68455dcbab424a
Tom Duszyk <tduszyk@gmail.com>
2020-01-25 11:24:17 -0500

    check_ssh_to_the_host

--- main.py
+++ main.py
@@ -9,26 +9,33 @@ no_access = []
 # Read files
 scope_file = open('scope.csv', 'r')
 hosts = scope_file.read().splitlines()
-#print(hosts)
 
-# Check if hosts are available by connecting to port 22
-for host in hosts:
-    # Create socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    result = sock.connect_ex((host, 22))
- 
-    if result == 0:
-    #    print(host)
-        access.append(host)
-        sock.shutdown(2)
-        sock.close()
-    else:
-    #    print(host)
-        no_access.append(host)
+def check_ssh_to_the_host(hosts):
 
-# print('SSH ', access)
-# print('noSSH', no_access)
+    # Check if hosts are available by connecting to port 22
+    for host in hosts:
+        # Create socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((host, 22))
+    
+        if result == 0:
+            #print(host)
+            access.append(host)
+            sock.shutdown(2)
+            sock.close()
+        else:
+            #print(host)
+            no_access.append(host)
+
+    # print('SSH ', access)
+    # print('noSSH', no_access)
+    return access,no_access
 
 # Check if Premium subscription is enabled
-for server in access:
-    print server
+# for server in access:
+#     print server
+
+check_ssh_to_the_host(hosts)
+
+print(access)
+print(no_access)
