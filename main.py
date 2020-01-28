afa21b565831fae9a47c769e02b2a3cd22374894
Tom Duszyk <tduszyk@gmail.com>
2020-01-28 07:54:42 -0500

    threading

--- main.py
+++ main.py
@@ -10,35 +10,35 @@ no_access = []
 scope_file = open('scope.csv', 'r')
 hosts = scope_file.read().splitlines()
 
-def check_ssh_to_the_host(hosts):
+# function to check ssh connection to the host
+def ssh_check(host):
+    # access = []
+    # no_access = []
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex((host, 22))
+    if result == 0:
+        #print(host)
+        access.append(host)
+        sock.shutdown(2)
+        sock.close()
+    else:
+        #print(host)
+        no_access.append(host)
 
-    # Check if hosts are available by connecting to port 22
-    for host in hosts:
-        # Create socket
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((host, 22))
-    
-        if result == 0:
-            #print(host)
-            access.append(host)
-            sock.shutdown(2)
-            sock.close()
-        else:
-            #print(host)
-            no_access.append(host)
-
-    # print('SSH ', access)
-    # print('noSSH', no_access)
+    print('SSH ', access)
+    print('noSSH', no_access)
     return access,no_access
 
-# Check if Premium subscription is enabled
-# for server in access:
-#     print server
-
-for name in range(len(hosts)):
-
-
-check_ssh_to_the_host(hosts)
+# multithread ssh_check execution
+threads = list()
+for index in range(len(hosts)):
+    t = threading.Thread(target=ssh_check, args=(hosts[index],))
+    threads.append(t)
+    t.start()
+    
 
 print(access)
 print(no_access)
+
+
+test
