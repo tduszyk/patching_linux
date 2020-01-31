c1d44479cfeae1c522272841a3daa6b919bc4347
Tom Duszyk <tduszyk@gmail.com>
2020-01-31 16:30:52 -0500

    repos_check

--- subscription_check.py
+++ subscription_check.py
@@ -14,3 +14,24 @@ subscriptions = scope_file.read().splitlines()
 # for  in subscriptions:
 #     print (host)
 
+import subprocess
+
+access = [] #add some IPs here
+
+
+proc1 = subprocess.Popen(['subscription-manager', 'list'], stdout=subprocess.PIPE)
+proc2 = subprocess.Popen(['grep', 'ARed Hat Enterprise Linux Server - Extended Update Support'], stdin=proc1.stdout,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+proc1.stdout.close() # Allow proc1 to receive a SIGPIPE if proc2 exits.
+out, err = proc2.communicate()
+
+#if True:
+#       print('out: {0}'.format(out))
+#else:
+#       print('err: {0}'.format(err))
+
+if not out:
+        print ('Re-REGISTER!!')
+else:
+        print('out: {0}'.format(out))
