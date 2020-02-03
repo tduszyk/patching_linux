d8508b7a80478e75ecc37aa573bd1f42d60a0ff2
Tom Duszyk <tduszyk@gmail.com>
2020-02-03 15:54:52 -0500

    repos

--- subscription_check.py
+++ subscription_check.py
@@ -1,4 +1,5 @@
 import os, re
+import subprocess
 
 # Variables
 subscribed = []
@@ -9,12 +10,7 @@ pattern = 'Red Hat Enterprise Linux Server - Extended Update Support'
 subscriptions_file = open('subscription-manager-output.txt', 'r')
 subscriptions = scope_file.read().splitlines()
 
-# Function to check if server is subscribed to RH Satellite
-#def satellite_check():
-# for  in subscriptions:
-#     print (host)
 
-import subprocess
 
 access = [] #add some IPs here
 
@@ -25,11 +21,6 @@ proc2 = subprocess.Popen(['grep', 'ARed Hat Enterprise Linux Server - Extended U
 
 proc1.stdout.close() # Allow proc1 to receive a SIGPIPE if proc2 exits.
 out, err = proc2.communicate()
-
-#if True:
-#       print('out: {0}'.format(out))
-#else:
-#       print('err: {0}'.format(err))
 
 if not out:
         print ('Re-REGISTER!!')
