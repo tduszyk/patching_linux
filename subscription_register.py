0579a63bc7183cb64328c46cd694495d9489f15d
Tom Duszyk <tduszyk@gmail.com>
2020-02-18 14:27:00 -0500

    subscription re-reg

--- subscription_register.py
+++ subscription_register.py
@@ -7,12 +7,11 @@ registered = ()
 failed = ()
 
 
-command_scp = ""
-command_register = ""
+command_register = "/sbin/subscription-manager unregister && /sbin/subscription-manager clean && /bin/yum remove -y katello* && /bin/curl -Sks http://ftc-lbkickst702.ad.moodys.net/redhat_satellite6/redhat_reg_sat6.sh | /bin/bash"
 
 for name in access:
 
-    ssh = subprocess.Popen(["ssh", "%s" % access, command],
+    ssh = subprocess.Popen(["ssh", "%s" % access, command_register],
                             shell=False,
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE)
