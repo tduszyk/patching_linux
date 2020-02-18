38f2b2abf57cd8f8a8dd836aaf9e4d86bd0e0feb
Tom Duszyk <tduszyk@gmail.com>
2020-02-18 15:21:42 -0500

    subscription re-reg

--- subscription_register.py
+++ subscription_register.py
@@ -6,18 +6,25 @@ access = () #add some IPs here
 registered = ()
 failed = ()
 
+# Function to re-register server to Satellite
+def rereg(server)
+    command_reregister = "/sbin/subscription-manager unregister && /sbin/subscription-manager clean && /bin/yum remove -y katello* && /bin/curl -Sks http://ftc-lbkickst702.ad.moodys.net/redhat_satellite6/redhat_reg_sat6.sh | /bin/bash"
 
-command_register = "/sbin/subscription-manager unregister && /sbin/subscription-manager clean && /bin/yum remove -y katello* && /bin/curl -Sks http://ftc-lbkickst702.ad.moodys.net/redhat_satellite6/redhat_reg_sat6.sh | /bin/bash"
-
-for name in access:
-
-    ssh = subprocess.Popen(["ssh", "%s" % access, command_register],
+    ssh = subprocess.Popen(["ssh", "%s" % server, command_reregister],
                             shell=False,
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE)
     result = ssh.stdout.readlines()
-    if result == [];
+    if result == []:
         error = ssh.stderr.readlines()
         print >> sys.stderr, "ERROR: %s" % error
     else:
         pprint(result)
+
+# multithread re-register function execution
+threads = list()
+for index in range(len(hosts)):
+    t = threading.Thread(target=ssh_check, args=(hosts[index],))
+    threads.append(t)
+    t.start()
+    t.join()  
